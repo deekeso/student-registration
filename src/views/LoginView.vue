263d21e5b6d6a8e274d15c54c7a1696378db7657
PLUS\Coleen.Garcia <Coleen.Garcia@tbu.net>
2025-03-26 09:58:54 +0800

    update log in route

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,71 @@
-<script setup lang="ts">
-import LoginForm from '@/components/LoginForm.vue'
+<template>
+  <div class="background">
+    <el-form class="form-container">
+      <el-form-item :model="loginForm">
+        <el-input
+          v-model="loginForm.username"
+          type="input"
+          placeholder="USERNAME"
+          class="user-input"
+          :prefix-icon="User"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          v-model="loginForm.passInput"
+          type="password"
+          show-password
+          class="user-input"
+          placeholder="PASSWORD"
+          :prefix-icon="Lock"
+        ></el-input>
+      </el-form-item>
+
+      <el-button type="plain" width="100%" @click="onSubmit">LOGIN</el-button>
+      <el-form-item>
+        <el-text class="mx-1">Forgot Password?</el-text>
+      </el-form-item>
+    </el-form>
+  </div>
+
+  <img src="../assets/images/Vector.svg" aria-disabled="true" class="bg-1 bg" />
+  <img src="../assets/images/Ellipse.svg" aria-disabled="true" class="ellipse bg" />
+  <img src="../assets/images/Ellipse-1.svg" aria-disabled="true" class="ellipse bg" />
+  <img src="../assets/images/Ellipse-2.svg" aria-disabled="true" class="ellipse bg" />
+</template>
+
+<script lang="ts" setup>
+// import { ref } from 'vue'
+import { reactive } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const loginForm = reactive({
+  username: '',
+  passInput: '',
+})
+
+const loginData: {
+  username: string
+  password: string
+} = {
+  username: 'root',
+  password: 'root',
+}
+
+function onSubmit() {
+  if (loginForm.username === loginData.username && loginForm.passInput === loginData.password) {
+    router.push('/register')
+  } else {
+    console.error('incorrect username or password')
+  }
+}
 </script>
 
-<template>
-  <LoginForm></LoginForm>
-</template>
+<style scoped>
+.el-input__wrapper,
+.form-container {
+  background-color: transparent;
+}
+</style>
